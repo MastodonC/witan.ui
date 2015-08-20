1bb3f5f26cb5e6e6fe268729c882e9ff139e0c17
Antony Woods <acron1@gmail.com>
2015-08-20 11:41:41 +0100

    Added comment to util fn

--- src/witan/ui/util.cljs
+++ src/witan/ui/util.cljs
@@ -2,9 +2,14 @@
     (:require [witan.ui.data :refer [app-state]]))
 
 (defn get-string
+  "Assumes that strings are always in the :strings keyword"
   [keyword]
   (-> @app-state :strings keyword))
 
-(defn prependtial [f & args1]
+(defn prependtial
+  "Works like `partial` except the additional args are prepended, rather than appended.
+   i.e. ((partial str 'hello') 'world') => 'helloworld'
+        ((prependtial str 'hello') 'world) => 'worldhello'"
+  [f & args1]
   (fn [& args2]
     (apply f (concat args2 args1))))
