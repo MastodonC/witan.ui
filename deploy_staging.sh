2f6932884a60920d508f6e607348bd6c858da838
Elise Huard <elise@jabberwocky.eu>
2015-09-14 13:50:16 +0200

    deployment security

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -5,7 +5,7 @@ TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 sed "s/@@TAG@@/$TAG/" witan-ui.json.template > witan-ui.json
 
 # we want curl to output something we can use to indicate success/failure
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://***REMOVED***:9501/marathon/witan-ui -H "Content-Type: application/json" -H "***REMOVED***: 123" --data-binary "@witan-ui.json")
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://***REMOVED***:9501/marathon/witan-ui -H "Content-Type: application/json" -H "$SEKRIT_HEADER: 123" --data-binary "@witan-ui.json")
 echo "HTTP code " $STATUS
 if [ $STATUS == "201" ]
 then exit 0
