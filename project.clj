42883c1b38ca3eb58489d96edc617547cc5481bd
Antony Woods <antony@mastodonc.com>
2015-11-19 17:09:59 +0000

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject witan.ui "0.1.0-SNAPSHOT"
+(defproject witan.ui "0.1.0"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
