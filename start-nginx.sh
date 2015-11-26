14f54ced49d3ba3e26df44cf55bc5b7cb719a471
Antony <acron1@gmail.com>
2015-11-26 16:38:35 +0000

    Update start-nginx.sh

--- start-nginx.sh
+++ start-nginx.sh
@@ -48,7 +48,7 @@ server {
         location /swagger.json {
             real_ip_header X-Forwarded-For;
             set_real_ip_from 0.0.0.0/0;
-            proxy_pass http://localhost:3000;
+            proxy_pass http://${SERVER_ADDR}:${SERVER_PORT};
         }
 
         location / {
@@ -56,7 +56,7 @@ server {
         }
 }
 EOF
-
+proxy_pass http://${SERVER_ADDR}:${SERVER_PORT}
 rm /etc/nginx/sites-enabled/*
 
 ln -sf ${PROXY_CONFIG_FILE} /etc/nginx/sites-enabled/default
