144661b1b3730c3b70aac92b0b73c323dffad302
Antony <acron1@gmail.com>
2015-11-26 16:49:56 +0000

    Added \ to escape $1

--- start-nginx.sh
+++ start-nginx.sh
@@ -36,9 +36,10 @@ server {
             real_ip_header X-Forwarded-For;
             set_real_ip_from 0.0.0.0/0;
 
-            rewrite ^/api-docs/(.+)$ /$1 break;
+            rewrite ^/api-docs/(.+)$ /\$1 break;
             proxy_pass http://${SERVER_ADDR}:${SERVER_PORT};        
         }
+        
         location /api-docs/ {
             real_ip_header X-Forwarded-For;
             set_real_ip_from 0.0.0.0/0;
