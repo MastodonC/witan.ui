933028da11593ec5461c11a42e88a01bb7e104cc
Antony Woods <antony@mastodonc.com>
2015-12-15 13:45:30 +0000

    Added python script to check for handler pairs

---
+++ scripts/check-failures.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import os
+import subprocess
+import re
+
+DATA_DIR=os.path.dirname(os.path.realpath(__file__))
+SRC_DIR=os.path.join(DATA_DIR, "..", "src")
+
+# grep
+proc = subprocess.Popen(["grep",
+                               "-orE",
+                               "\[:[a-z-]+\s:(success|failure)\]",
+                                SRC_DIR],
+                               stdout=subprocess.PIPE)
+
+# regex
+line_regex = re.compile(ur'^(.*.cljs):\[(:[a-z-]+)\s:(success|failure)')
+results = []
+for line in iter(proc.stdout.readline,''):
+    results.append(re.search (line_regex , line.rstrip().replace(SRC_DIR, '')).groups ())
+
+groups = {}
+for entry in results:
+    if not groups.has_key(entry[0]):
+        groups[entry[0]] = []
+    groups[entry[0]].append(entry[1:])
+
+pairs = {}
+for entry in groups:
+    handles = groups[entry]
+    for handle, status in handles:
+        check = [x for x in handles if x[0] == handle]
+        if len(check) != 2:
+            if not pairs.has_key(entry):
+                pairs[entry] = []
+            pairs[entry].append(check)
+
+# print
+print "Results - the following calls do NOT have complete :success/:failure pairs:"
+print "--------------------------------------------------------"
+for key in pairs:
+    print key
+    for handles in pairs[key]:
+        print " - ", handles
