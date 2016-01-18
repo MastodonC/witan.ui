3abc1fbc79c14b35809f02a1c068bb6df90af4ab
Antony Woods <antony@mastodonc.com>
2016-01-18 10:29:52 +0000

    Upgrade to venue 0.1.10-SNAPSHOT and add subscribe
    Page changes are now published by venue

--- src/cljs/witan/ui/services/analytics.cljs
+++ src/cljs/witan/ui/services/analytics.cljs
@@ -1,16 +1,35 @@
 (ns witan.ui.services.analytics
   (:require [witan.ui.util :as util]
+            [cljs.core.async :refer [close!]]
             [venue.core :as venue]
             [cljs-time.core :as t]
             [cljs-time.format :as tf])
   (:require-macros [cljs-log.core :as log]
                    [witan.ui.macros :as wm]))
 
-(wm/create-standard-service!)
+(def analytics-state (atom {}))
 
 (defn log-event
   [message]
   (log/debug "Analytics:" message))
+
+(defn on-initialise
+  []
+  (log-event "System initialised."))
+
+(defn request-handler
+  [event args result-ch]
+  (close! result-ch))
+
+(defn service
+  []
+  (reify
+    venue/IHandleRequest
+    (handle-request [owner request args response-ch]
+      (request-handler request args response-ch))
+    venue/IInitialise
+    (initialise [owner _]
+      (on-initialise))))
 
 ;;;;;;;;;;;;;;;;;;;;;
 
@@ -30,6 +49,14 @@
   (log-event "Switching OFF Intercom")
   (.Intercom js/window "shutdown"))
 
+(defn change-view
+  [{:keys [target id args] :as view-event}]
+  (when (and (= target :app) (not= (:view-event @analytics-state) view-event))
+    (swap! analytics-state #(assoc % :view-event view-event))
+    (.Intercom js/window "trackEvent" "pageChange")))
+
+;;;;;;;;;;;;;;;;;;;;;
+
 (util/inline-subscribe!
  :api/user-identified
  #(do-login true %))
@@ -37,3 +64,7 @@
 (util/inline-subscribe!
  :api/user-logged-out
  #(do-login false))
+
+(util/inline-subscribe!
+ :venue/view-activated
+ #(change-view %))
