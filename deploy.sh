5a687f2e27ef8831d34ac96c06dc5b079cfdba83
Elise Huard <elise@jabberwocky.eu>
2016-01-26 16:24:15 +0000

    continuous deployment staging

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-# using deployment service sebastopol
+SEBASTOPOL_IP=$1
+
+# deployment service sebastopol
 TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
+TAG=latest
 sed "s/@@TAG@@/$TAG/" witan-ui.json.template > witan-ui.json
 
 # we want curl to output something we can use to indicate success/failure
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://$DEPLOY_IP:9501/marathon/witan-ui -H "Content-Type: application/json" -H "$SEKRIT_HEADER: 123" --data-binary "@witan-ui.json")
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://$SEBASTOPOL_IP:9501/marathon/witan-ui -H "Content-Type: application/json" -H "$SEKRIT_HEADER: 123" --data-binary "@witan-ui.json")
 echo "HTTP code " $STATUS
 if [ $STATUS == "201" ]
 then exit 0
